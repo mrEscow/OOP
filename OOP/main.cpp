6ae4d024e1750cada44a3915207c4f5bac3af21e
mr.Escow <escowxbox@gmail.com>
2021-07-21 22:48:51 +0300

    OK

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -1,4 +1,5 @@
-
+#include <iostream>
+#include <cmath>
 
 
 
@@ -11,12 +12,12 @@
 const float PI = 3.141592653589;
 
 class Figure {
-protected:
-	Figure();
+public:
+	//Figure();
 	virtual float area() = 0;
 };
 
-class Parallelogram : protected Figure {
+class Parallelogram : public Figure {
 private:
 	float m_width{ 0 };
 	float m_height{ 0 }; 
@@ -29,7 +30,7 @@ public:
 	}
 };
 
-class Circle : protected Figure {
+class Circle : public Figure {
 private:
 	float m_diameter{ 0 };
 public:
@@ -41,26 +42,60 @@ public:
 	}
 };
 
-class Rectangle : protected Parallelogram {
-	Rectangle();
+class Rectangle : public  Parallelogram {
+private:
+	float m_width{ 0 };
+	float m_height{ 0 };
+public:
+	Rectangle(float width, float height): Parallelogram(width, height) {
+		m_width = width;
+		m_height = height;
+	}
 	float area() override {
-
+		return m_width * m_height; 
 	}
 };
 
-class Square : protected Parallelogram {
-	Square();
+class Square : public Parallelogram {
+private:
+	float m_side{ 0 };
+public:
+	Square(float side) : Parallelogram(side, side), m_side(side) {}
 	float area() override {
-
+		return m_side * m_side;
 	}
 };
 
-class Rhombus : protected Parallelogram {
-	Rhombus();
+class Rhombus : public Parallelogram {
+private:
+	float m_side{ 0 };
+	float m_angle{ 0 };
+public:
+	Rhombus(float side,float angle):Parallelogram(side,side),m_side(side),m_angle(angle){}
 	float area() override {
-
+		std::cout << sin(m_angle)<< std::endl;
+		return (m_side * m_side) * sin(m_angle);
 	}
 };
 int main() {
+
+	Parallelogram parallelogram(15, 10);
+	Circle circle(20);
+	Rectangle rectangle(8, 20);
+	Square square(17);
+	Rhombus rhombus(5, 45);
+
+	Figure* figure;
+
+	figure = &parallelogram;
+	std::cout << figure->area() << std::endl;
+	figure = &circle;
+	std::cout << figure->area() << std::endl;
+	figure = &rectangle;
+	std::cout << figure->area() << std::endl;
+	figure = &square;
+	std::cout << figure->area() << std::endl;
+	figure = &rhombus;
+	std::cout << figure->area() << std::endl;
 	return 0;
 }
