955939214831dc062973d37d561c202193c7efbe
mr.Escow <escowxbox@gmail.com>
2021-07-25 14:10:01 +0300

    OK

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -1,3 +1,32 @@
-int main() {
-	return 0;
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+
+int main()
+{
+    int* a = nullptr;
+
+    cout << a << endl;
+
+    if (a) {
+        cout << "not nullptr" << endl;
+    }
+    else {
+        cout << "nullptr" << endl;
+    }
+
+    if (a == nullptr) {
+        cout << "nullptr" << endl;
+    }
+    else{
+        cout << "not nullptr" << endl;
+    }
+    return 0;
 }
+
+
+
