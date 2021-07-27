f25f2fcbe8631c457eb8c3675980dc115c1f770e
mr.Escow <escowxbox@gmail.com>
2021-07-27 21:02:38 +0300

    OK2

--- OOP/Lesson_5.h
+++ OOP/Lesson_5.h
@@ -12,7 +12,6 @@ public:
      T  first()   const { return  m_data_1; }
      T  second()  const { return  m_data_2; }
 
-
 };
 
 template<typename T>
@@ -22,16 +21,46 @@ inline Pair1<T>::Pair1(T data1,T data2)
     m_data_2 = data2;
 }
 
+template<typename T1, typename T2>
+class Pair {
+private:
+    T1 m_data_1;
+    T2 m_data_2;
+public:
+    Pair(T1 data1, T2 data2);
+    //~Pair();
+
+    T1  first()   const { return  m_data_1; }
+    T2  second()  const { return  m_data_2; }
+
+};
+
+template<typename T1, typename T2>
+inline Pair<T1, T2>::Pair(T1 data1, T2 data2)
+{
+    m_data_1 = data1;
+    m_data_2 = data2;
+}
+
 void Lesson_5() {
-	cout << "Lesson_5\n" << endl;
+    cout << "Lesson_5" << endl;
+    cout << "----------------" << endl;
+    {
+        Pair1<int> p1(6, 9);
+        cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
 
-    Pair1<int> p1(6, 9);
-    cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+        const Pair1<double> p2(3.4, 7.8);
+        cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+    }
+    cout << "----------------" << endl;
+    {
+        Pair<int, double> p1(6, 7.8);
+        cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
 
-    const Pair1<double> p2(3.4, 7.8);
-    cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
-
-    
+        const Pair<double, int> p2(3.4, 5);
+        cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+    }
+    cout << "----------------" << endl;
 }
 
 
