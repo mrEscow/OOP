79dce47f58b920300720d501aeef3f9c85a44e91
mr.Escow <escowxbox@gmail.com>
2021-07-27 21:46:31 +0300

    OK3

--- OOP/Lesson_5.h
+++ OOP/Lesson_5.h
@@ -1,5 +1,9 @@
 #pragma once
 
+//------------------------------------------------------------------------------------------------------
+// Реализовать шаблон класса Pair1, который позволяет пользователю передавать данные одного типа парами.
+//------------------------------------------------------------------------------------------------------
+
 template<typename T>
 class Pair1 {
 private:
@@ -8,9 +12,11 @@ private:
 public:
     Pair1(T data1, T data2);
     //~Pair1();
+    T  first()    { return  m_data_1; }
+    T  second()   { return  m_data_2; }
 
-     T  first()   const { return  m_data_1; }
-     T  second()  const { return  m_data_2; }
+    T  first()   const { return  m_data_1; }
+    T  second()  const { return  m_data_2; }
 
 };
 
@@ -20,6 +26,10 @@ inline Pair1<T>::Pair1(T data1,T data2)
     m_data_1 = data1;
     m_data_2 = data2;
 }
+
+//------------------------------------------------------------------------------------------------
+// Реализовать класс Pair, который позволяет использовать разные типы данных в передаваемых парах.
+//------------------------------------------------------------------------------------------------
 
 template<typename T1, typename T2>
 class Pair {
@@ -42,13 +52,27 @@ inline Pair<T1, T2>::Pair(T1 data1, T2 data2)
     m_data_2 = data2;
 }
 
+//--------------------------------------------------------------------------------------------------------------
+// Написать шаблон класса StringValuePair, в котором первое значение всегда типа string, а второе — любого типа.
+// Этот шаблон класса должен наследовать частично специализированный класс Pair, 
+// в котором первый параметр — string, а второй — любого типа данных.
+//--------------------------------------------------------------------------------------------------------------
+
+template<typename Type>
+class StringValuePair : public Pair<string, Type> {
+public:
+    StringValuePair(string str, Type data) : Pair<string, Type> (str, data){}
+};
+
+// Подсказка: при вызове конструктора класса Pair из конструктора класса StringValuePair не забудьте указать, что параметры относятся к классу Pair.
+
 void Lesson_5() {
     cout << "Lesson_5" << endl;
     cout << "----------------" << endl;
     {
         Pair1<int> p1(6, 9);
         cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
-
+        
         const Pair1<double> p2(3.4, 7.8);
         cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
     }
@@ -59,6 +83,11 @@ void Lesson_5() {
 
         const Pair<double, int> p2(3.4, 5);
         cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+    }
+    cout << "----------------" << endl;
+    {
+        StringValuePair<int> svp("Amazing", 7);
+        std::cout << "Pair: " << svp.first() << ' ' << svp.second() << '\n';
     }
     cout << "----------------" << endl;
 }
