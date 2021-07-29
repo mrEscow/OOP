6fbf27bd1a1ee03e517751cc520d2a807431bc4b
mr.Escow <escowxbox@gmail.com>
2021-07-29 13:26:59 +0300

    Создана структура курса

---
+++ OOP/Header.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <clocale>
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <cstdint>
+
+using namespace std; 
+
+#include "Metoda.h" 
+
+#include "Lesson_1.h"
+#include "Lesson_2.h"
+#include "Lesson_3.h"
+#include "Lesson_4.h"
+#include "Lesson_5.h"
+#include "Lesson_6.h"
+#include "Lesson_7.h"
+#include "Lesson_8.h"
+
+#include "Blackjack.h"  
