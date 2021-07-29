22f64854c1a8565ff47a13608cb7b74185beeda7
mr.Escow <escowxbox@gmail.com>
2021-07-29 15:31:04 +0300

    Update Lesson_1

--- OOP/Lesson_1.h
+++ OOP/Lesson_1.h
@@ -1,130 +1,144 @@
 #pragma once
-class Person {
-protected:
-    std::string m_name;
-    std::string m_gender;
-    int m_age;
-    float m_weight;
-public:
-    Person(std::string name, std::string gender, int age, float weight)
-        : m_gender(gender), m_name(name), m_age(age), m_weight(weight)
-    {
 
-    };
+//--------------------------------------------------------------------------------------------------------
+//	Создать класс Power, который содержит два вещественных числа.
+//	Этот класс должен иметь две переменные - члена для хранения этих вещественных чисел.
+//	Еще создать два метода : 
+//	один с именем set, который позволит присваивать значения переменным, 
+//	второй — calculate, который будет выводить результат возведения первого числа в степень второго числа.
+//	Задать значения этих двух чисел по умолчанию.
+//--------------------------------------------------------------------------------------------------------
 
-    void SetName(std::string name) {
-        m_name = name;
-    }
-    void SetAge(int age) {
-        m_age = age;
-    }
-    void SetWeight(float weight) {
-        m_weight = weight;
-    }
-};
 
-class Student : public Person {
+class Power
+{
 private:
-    int m_yearsStudy{ 0 };
+
+	float m_one{ 0 };
+	float m_two{ 0 };
+
 public:
-    static int m_count;
-    Student(std::string name, std::string gender, int age, float weight, int yearsStudy)
-        :Person(name, gender, age, weight), m_yearsStudy(yearsStudy)
-    {
-        m_count++;
-    };
 
-    void SetYearsStudy(int yearsStudy) {
-        m_yearsStudy = yearsStudy;
-    }
-
-    void SetYearsStudy() {
-        m_yearsStudy++;
-    }
-
-    friend void PrintStydent(const Student& student);
+	void SetValue(float one, float two) {
+		m_one = one;
+		m_two = two;
+	}
+	float Calculate() {
+		return pow(m_one, m_two);
+	}
 };
 
+//------------------------------------------------------------------------------------------
+//	Написать класс с именем RGBA, который содержит 4 переменные - члена типа std::uint8_t: 
+//	m_red, m_green, m_blue и m_alpha(#include cstdint для доступа к этому типу).
+//	Задать 0 в качестве значения по умолчанию для m_red, m_green, m_blue и 255 для m_alpha.
+//	Создать конструктор со списком инициализации членов, 
+//	который позволит пользователю передавать значения для m_red, m_blue, m_green и m_alpha.
+//	Написать функцию print(), которая будет выводить значения переменных - членов.
+//------------------------------------------------------------------------------------------
 
-int Student::m_count{ 0 };
+class RGBA
+{
+private:
 
-void PrintStydent(const Student& student) {
-    std::cout << "Student:" << std::endl;
-    std::cout << "Name:\t" << student.m_name << std::endl;
-    std::cout << "Gender:\t" << student.m_gender << std::endl;
-    std::cout << "Age:\t" << student.m_age << std::endl;
-    std::cout << "Weight:\t" << student.m_weight << std::endl;
-    std::cout << "Years of study:\t" << student.m_yearsStudy << std::endl;
+	std::uint8_t
+		m_red{ 0 },
+		m_green{ 0 },
+		m_blue{ 0 },
+		m_alpha{ 255 };
+
+public:
+
+	RGBA(std::uint8_t red, std::uint8_t green, std::uint8_t blue, std::uint8_t alpha)
+		: m_red(red), m_green(green), m_blue(blue), m_alpha(alpha)
+	{
+
+	};
+
+	void print() {
+		std::cout << "RED\t" << m_red << std::endl;
+		std::cout << "GREEN\t" << m_green << std::endl;
+		std::cout << "BLUE\t" << m_blue << std::endl;
+		std::cout << "ALPHA\t" << m_alpha << std::endl;
+	}
+};
+
+//------------------------------------------------------------------------------------------
+//	Написать класс, который реализует функциональность стека.Класс Stack должен иметь :
+//	private - массив целых чисел длиной 10;
+//	private целочисленное значение для отслеживания длины стека;
+//	public - метод с именем reset(), 
+//	который будет сбрасывать длину и все значения элементов на 0;
+//	public - метод с именем push(), 
+//	который будет добавлять значение в стек.push() 
+//	должен возвращать значение false, если массив уже заполнен, и true в противном случае;
+//	public - метод с именем pop() для вытягивания и возврата значения из стека.
+//	Если в стеке нет значений, то должно выводиться предупреждение;
+//	public - метод с именем print(), который будет выводить все значения стека.
+//------------------------------------------------------------------------------------------
+
+class Stack
+{
+private:
+	int m_array[10]{ 0 };
+	int m_length{ 0 };
+public:
+	void reset();
+	bool push(int value);
+	void pop();
+	void print();
+};
+
+void Stack::reset() {
+	m_length = 0;
+	for (int i = 0; i < 10; ++i) {
+		m_array[i] = 0;
+	}
 }
 
-//--------------------------------------------------------------------------------------------------------
+bool Stack::push(int value) {
+	if (m_length > 9) {
+		std::cout << "STACK IS OVER" << std::endl;
+		return false;
+	}
+	else {
+		m_array[m_length] = value;
+		m_length++;
+		return true;
+	}
+}
 
-class Fruit {
+void Stack::pop() {
+	if (m_length == -1) {
+		std::cout << "STACK IS EMPTY" << std::endl;
+	}
+	else {
+		m_array[m_length] = 0;
+		m_length--;
+	}
+}
 
-    std::string m_name;
-    std::string m_color;
+void Stack::print() {
+	std::cout << "(";
+	for (int i = 0; i < m_length; ++i) {
+		std::cout << " " << m_array[i] << " ";
+	}
+	std::cout << ")" << std::endl;
+}
 
-public:
-
-    Fruit(std::string name, std::string color) {
-        m_name = name;
-        m_color = color;
-    }
-
-    std::string getName() {
-        return m_name;
-    }
-
-    std::string getColor() {
-        return m_color;
-    }
-};
-
-class Apple : public Fruit {
-public:
-    Apple(std::string color, std::string name = "apple") : Fruit(name, color) {
-
-    }
-};
-
-class Banana : public Fruit {
-public:
-    Banana() :Fruit("banana", "yellow") {
-
-    }
-};
-
-class GrannySmith : public Apple {
-public:
-    GrannySmith() :Apple("green", "Granny Smith") {};
-};
-
-//--------------------------------------------------------------------------------------------------------
-
-
+//-------------
 
 void Lesson_1() {
-    //--------------------------------------------------------------------------------------------------------
-    std::cout << "-------------------" << std::endl;
-    Student ivan("Ivan", "man", 18, 70, 2);
-    Student vasia("Vasia", "man", 20, 80, 4);
-    Student petia("Ivan", "man", 23, 65, 6);
-    PrintStydent(vasia);
-    //--------------------------------------------------------------------------------------------------------
-    std::cout << "-------------------" << std::endl;
-    Apple a("red");
-    Banana b;
-    GrannySmith c;
-
-    std::cout << "My " << a.getName() << " is " << a.getColor() << ".\n";
-    std::cout << "My " << b.getName() << " is " << b.getColor() << ".\n";
-    std::cout << "My " << c.getName() << " is " << c.getColor() << ".\n";
-    //--------------------------------------------------------------------------------------------------------
-    std::cout << "-------------------" << std::endl;
-
-    std::cout << "https://add-hobby.ru/blackjack.html" << std::endl;
-    std::cout << "https://www.cyberforum.ru/cpp-beginners/thread89417.html" << std::endl;
-
-
-    //--------------------------------------------------------------------------------------------------------
+	Stack stack;
+	stack.reset();
+	stack.print();
+	stack.push(3);
+	stack.push(7);
+	stack.push(5);
+	stack.print();
+	stack.pop();
+	stack.print();
+	stack.pop();
+	stack.pop();
+	stack.print();
 }
--- OOP/Lesson_2.h
+++ OOP/Lesson_2.h
@@ -1,117 +1,130 @@
 #pragma once
+class Person {
+protected:
+    std::string m_name;
+    std::string m_gender;
+    int m_age;
+    float m_weight;
+public:
+    Person(std::string name, std::string gender, int age, float weight)
+        : m_gender(gender), m_name(name), m_age(age), m_weight(weight)
+    {
 
-class Power
-{
+    };
+
+    void SetName(std::string name) {
+        m_name = name;
+    }
+    void SetAge(int age) {
+        m_age = age;
+    }
+    void SetWeight(float weight) {
+        m_weight = weight;
+    }
+};
+
+class Student : public Person {
 private:
+    int m_yearsStudy{ 0 };
+public:
+    static int m_count;
+    Student(std::string name, std::string gender, int age, float weight, int yearsStudy)
+        :Person(name, gender, age, weight), m_yearsStudy(yearsStudy)
+    {
+        m_count++;
+    };
 
-	float m_one{ 0 };
-	float m_two{ 0 };
+    void SetYearsStudy(int yearsStudy) {
+        m_yearsStudy = yearsStudy;
+    }
+
+    void SetYearsStudy() {
+        m_yearsStudy++;
+    }
+
+    friend void PrintStydent(const Student& student);
+};
+
+
+int Student::m_count{ 0 };
+
+void PrintStydent(const Student& student) {
+    std::cout << "Student:" << std::endl;
+    std::cout << "Name:\t" << student.m_name << std::endl;
+    std::cout << "Gender:\t" << student.m_gender << std::endl;
+    std::cout << "Age:\t" << student.m_age << std::endl;
+    std::cout << "Weight:\t" << student.m_weight << std::endl;
+    std::cout << "Years of study:\t" << student.m_yearsStudy << std::endl;
+}
+
+//--------------------------------------------------------------------------------------------------------
+
+class Fruit {
+
+    std::string m_name;
+    std::string m_color;
 
 public:
 
-	void SetValue(float one, float two) {
-		m_one = one;
-		m_two = two;
-	}
-	float Calculate() {
-		return pow(m_one, m_two);
-	}
+    Fruit(std::string name, std::string color) {
+        m_name = name;
+        m_color = color;
+    }
+
+    std::string getName() {
+        return m_name;
+    }
+
+    std::string getColor() {
+        return m_color;
+    }
 };
 
-class RGBA
-{
-private:
-
-	std::uint8_t
-		m_red{ 0 },
-		m_green{ 0 },
-		m_blue{ 0 },
-		m_alpha{ 255 };
-
+class Apple : public Fruit {
 public:
+    Apple(std::string color, std::string name = "apple") : Fruit(name, color) {
 
-	RGBA(std::uint8_t red, std::uint8_t green, std::uint8_t blue, std::uint8_t alpha)
-		: m_red(red), m_green(green), m_blue(blue), m_alpha(alpha)
-	{
-
-	};
-
-	void print() {
-		std::cout << "RED\t" << m_red << std::endl;
-		std::cout << "GREEN\t" << m_green << std::endl;
-		std::cout << "BLUE\t" << m_blue << std::endl;
-		std::cout << "ALPHA\t" << m_alpha << std::endl;
-	}
+    }
 };
 
-class Stack
-{
-private:
-	int m_array[10]{ 0 };
-	int m_length{ 0 };
+class Banana : public Fruit {
 public:
-	void reset();
-	bool push(int value);
-	void pop();
-	void print();
+    Banana() :Fruit("banana", "yellow") {
+
+    }
 };
 
-void Stack::reset() {
-	m_length = 0;
-	for (int i = 0; i < 10; ++i) {
-		m_array[i] = 0;
-	}
-}
+class GrannySmith : public Apple {
+public:
+    GrannySmith() :Apple("green", "Granny Smith") {};
+};
 
-bool Stack::push(int value) {
-	if (m_length > 9) {
-		std::cout << "STACK IS OVER" << std::endl;
-		return false;
-	}
-	else {
-		m_array[m_length] = value;
-		m_length++;
-		return true;
-	}
-}
+//--------------------------------------------------------------------------------------------------------
 
-void Stack::pop() {
-	if (m_length == -1) {
-		std::cout << "STACK IS EMPTY" << std::endl;
-	}
-	else {
-		m_array[m_length] = 0;
-		m_length--;
-	}
-}
-
-void Stack::print() {
-	std::cout << "(";
-	for (int i = 0; i < m_length; ++i) {
-		std::cout << " " << m_array[i] << " ";
-	}
-	std::cout << ")" << std::endl;
-}
 
 
 void Lesson_2() {
+    //--------------------------------------------------------------------------------------------------------
+    std::cout << "-------------------" << std::endl;
+    Student ivan("Ivan", "man", 18, 70, 2);
+    Student vasia("Vasia", "man", 20, 80, 4);
+    Student petia("Ivan", "man", 23, 65, 6);
+    PrintStydent(vasia);
+    //--------------------------------------------------------------------------------------------------------
+    std::cout << "-------------------" << std::endl;
+    Apple a("red");
+    Banana b;
+    GrannySmith c;
 
-	Stack stack;
-	stack.reset();
-	stack.print();
+    std::cout << "My " << a.getName() << " is " << a.getColor() << ".\n";
+    std::cout << "My " << b.getName() << " is " << b.getColor() << ".\n";
+    std::cout << "My " << c.getName() << " is " << c.getColor() << ".\n";
+    //--------------------------------------------------------------------------------------------------------
+    std::cout << "-------------------" << std::endl;
 
-	stack.push(3);
-	stack.push(7);
-	stack.push(5);
-	stack.print();
-
-	stack.pop();
-	stack.print();
-
-	stack.pop();
-	stack.pop();
-	stack.print();
+    std::cout << "https://add-hobby.ru/blackjack.html" << std::endl;
+    std::cout << "https://www.cyberforum.ru/cpp-beginners/thread89417.html" << std::endl;
 
 
-
+    //--------------------------------------------------------------------------------------------------------
 }
